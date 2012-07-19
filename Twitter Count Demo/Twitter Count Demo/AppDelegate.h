04ca1d21c6ae029e7ea187c30682ab46c83aba51
Josh Hudnall <josh@methodapps.com>
2012-07-19 16:51:18 -0600

    Initial Commit

---
+++ Twitter Count Demo/Twitter Count Demo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Twitter Count Demo
+//
+//  Created by Josh Hudnall on 7/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
